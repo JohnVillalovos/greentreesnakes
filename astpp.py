ee0ea8762c690382da24c37bdd7eb8659cff09ff
MatthieuDartiailh <marul@laposte.net>
2020-01-29 18:52:58 -0500

    astpp: allow to keep type_comment in the AST

--- astpp.py
+++ astpp.py
@@ -7,6 +7,7 @@ Alex Leone (acleone ~AT~ gmail.com), 2010-01-30
 From http://alexleone.blogspot.co.uk/2010/01/python-ast-pretty-printer.html
 """
 
+import sys
 from ast import *
 
 def dump(node, annotate_fields=True, include_attributes=False, indent='  '):
@@ -41,14 +42,18 @@ def dump(node, annotate_fields=True, include_attributes=False, indent='  '):
                 lines[-1] += ']'
             return '\n'.join(lines)
         return repr(node)
-    
+
     if not isinstance(node, AST):
         raise TypeError('expected AST, got %r' % node.__class__.__name__)
     return _format(node)
 
-def parseprint(code, filename="<string>", mode="exec", **kwargs):
+def parseprint(code, filename="<string>", mode="exec", type_comments=False,
+               **kwargs):
     """Parse some code from a string and pretty-print it."""
-    node = parse(code, mode=mode)   # An ode to the code
+    if sys.version_info >= (3, 8):
+        node = parse(code, mode=mode, type_comments=type_comments)
+    else:
+        node = parse(code, mode=mode)   # An ode to the code
     print(dump(node, **kwargs))
 
 # Short name: pdp = parse, dump, print
@@ -57,24 +62,28 @@ pdp = parseprint
 def load_ipython_extension(ip):
     from IPython.core.magic import Magics, magics_class, cell_magic
     from IPython.core import magic_arguments
-    
+
     @magics_class
     class AstMagics(Magics):
-        
+
         @magic_arguments.magic_arguments()
         @magic_arguments.argument(
             '-m', '--mode', default='exec',
             help="The mode in which to parse the code. Can be exec (the default), "
                  "eval or single."
-        )    
+        )
+        @magic_arguments.argument(
+            '-t', '--type-comments', default=False,
+            help="Whether to preserve the type comments in the AST"
+        )
         @cell_magic
         def dump_ast(self, line, cell):
             """Parse the code in the cell, and pretty-print the AST."""
             args = magic_arguments.parse_argstring(self.dump_ast, line)
-            parseprint(cell, mode=args.mode)
-    
+            parseprint(cell, mode=args.mode, type_comments=args.type_comments)
+
     ip.register_magics(AstMagics)
-    
+
 if __name__ == '__main__':
     import sys, tokenize
     for filename in sys.argv[1:]:
@@ -83,6 +92,6 @@ if __name__ == '__main__':
         print('=' * 50)
         with tokenize.open(filename) as f:
             fstr = f.read()
-        
+
         parseprint(fstr, filename=filename, include_attributes=True)
         print()
